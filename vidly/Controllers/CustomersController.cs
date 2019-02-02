757d6006ef52afcb420c279876ec574e5d436302
Ha Tran <tranvietha.2809@gmail.com>
2019-02-02 13:16:55 +0200

    Intial commit
    Simple CRUD application

---
+++ vidly/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+namespace vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        IEnumerable<Customer> CustomerList = new List<Customer>
+        {
+            new Customer {Id = 1, Name = "John Smith"},
+            new Customer {Id = 2, Name = "Mary William"}
+        };
+
+        // GET: Customers
+        
+        public ActionResult Index()
+        {
+            return View(CustomerList);
+        }
+
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            foreach (var customer in CustomerList)
+            {
+                if (customer.Id == id)
+                {
+                    return View(customer);
+                }
+            }
+            return HttpNotFound();
+        }
+        /**private IEnumerable<Customer> GetCustomers()
+        {
+                return new List<Customer> 
+                {
+                    new Customer {Name = "John Smith", Id = 1 },
+                    new Customer { Name = "Mary Williams", Id = 2 }
+                };
+        };**/
+    }
+}
---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+using vidly.ViewModels;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name="Customer 1"},
+                new Customer {Name="Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+                
+            };
+            return View(viewModel);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return View();
+        }
+
+        public ActionResult edit(int id)
+        {
+            return Content("id = " + id);
+        }
+        
+    }
+}
